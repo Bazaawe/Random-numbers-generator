898f0ad3e61fd8f9bfbe9594f3f8a53fd6ca0a41
Bazaawe <latifmusahbazaawe@yahoo.com>
2023-09-13 23:52:02 +0200

    Using Python

---
+++ randomnums/main.py
@@ -0,0 +1,9 @@
+import random
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+target = 0
+
+while target < 6:    
+    target += 1
+    print(random.randrange(num1,num2), end=" ")
